072082c38199bc8e94edae7a753078f2480c766a
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-02-02 11:27:11 +0100

    update

--- app/backend/backend.py
+++ app/backend/backend.py
@@ -13,25 +13,7 @@ class Backend:
             database="store"
         )
         self.cursor = self.connection.cursor()
-
-    def add_product(self, name, description, price, quantity, id_category):
-        query = "INSERT INTO product (name, description, price, quantity, id_category) VALUES (%s, %s, %s, %s, %s)"
-        values = (name, description, price, quantity, id_category)
-        self.cursor.execute(query, values)
-        self.connection.commit()
-
-    def remove_product(self, product_id):
-        query = "DELETE FROM product WHERE id = %s"
-        values = (product_id,)
-        self.cursor.execute(query, values)
-        self.connection.commit()
-
-    def modify_product(self, product_id, new_values):
-        query = "UPDATE product SET name=%s, description=%s, price=%s, quantity=%s, id_category=%s WHERE id=%s"
-        values = (*new_values, product_id)
-        self.cursor.execute(query, values)
-        self.connection.commit()
-
+        
     def get_all_products(self):
         query = """
             SELECT product.id, product.name, product.description, 
@@ -41,6 +23,24 @@ class Backend:
         """
         self.cursor.execute(query)
         return self.cursor.fetchall()
+
+    def add_product(self, name, description, price, quantity, id_category):
+        query = "INSERT INTO product (name, description, price, quantity, id_category) VALUES (%s, %s, %s, %s, %s)"
+        values = (name, description, price, quantity, id_category)
+        self.cursor.execute(query, values)
+        self.connection.commit()
+    
+    def modify_product(self, product_id, new_values):
+        query = "UPDATE product SET name=%s, description=%s, price=%s, quantity=%s, id_category=%s WHERE id=%s"
+        values = (*new_values, product_id)
+        self.cursor.execute(query, values)
+        self.connection.commit()
+
+    def remove_product(self, product_id):
+        query = "DELETE FROM product WHERE id = %s"
+        values = (product_id,)
+        self.cursor.execute(query, values)
+        self.connection.commit()
 
     def close_connection(self):
         self.cursor.close()
